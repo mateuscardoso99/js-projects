df284147731ca136df9cde1f04131b31cc385227
Mateus Vargas <mat358591@gmail.com>
2022-07-12 13:12:06 -0300

    Update spread_operator-destructuring.js

--- spread_operator-destructuring.js
+++ spread_operator-destructuring.js
@@ -1,3 +1,18 @@
+// operador ... rest(juntar)/spread(espalhar)
+// usar rest com parâmetro de função
+
+// usar spread com objeto
+const funcionario = { nome: 'Maria', salario: 12348.99 }
+const clone = { ativo: true, ...funcionario }
+console.log(clone)//{ativo: true, nome: 'Maria', salario: 12348.99}
+
+// usar spread com array
+const grupoA = ['João', 'Pedro', 'Gloria']
+const grupoFinal = ['Maria', ...grupoA, 'Rafaela']
+console.log(grupoFinal)//['Maria', 'João', 'Pedro', 'Gloria', 'Rafaela']
+
+
+
 //objetos
 
 const pessoa = {
