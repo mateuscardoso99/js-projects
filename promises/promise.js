0874282e7838dd7ccfcb3a6f6f969e636e99b95b
Mateus Cardoso <mat358591@gmail.com>
2024-03-27 18:07:44 -0300

    Update promise.js

--- promises/promise.js
+++ promises/promise.js
@@ -29,6 +29,39 @@ console.log("sexto")
 //  - REJEITADA
 //  - ESTABELECIDA
 
+/**
+ * Usamos o new Promise para criar a promise,
+ * ela recebe como parâmetro uma função.
+ * Essa função têm dois parâmetros, resolve e
+ * reject.
+ * Usamos resolve quando queremos retornar o
+ * sucesso e reject quando queremos demonstrar
+ * a falha.
+ */
+const promise = new Promise((resolve, reject) => {
+  /**
+   * Adicionei um setTimeout para falar que
+   * essa promise precisa esperar 300
+   * milisegundos para retornar (resolve)
+   * o seu resultado
+   */
+  setTimeout(() => {
+    resolve('mundo!');
+  }, 300)
+});
+
+/**
+ * O .then() é um código padrão para quando
+ * a promise finalizar, com sucesso ou falha.
+ * Isso vai executar o que você quer fazer 
+ * com o resultado quando sua promise retornar
+ * o resultado esperado.
+ */
+promise.then(response => {
+  console.log(response);
+});
+
+
 
 function realizada(){
     return Promise.resolve(45)//podem ser passados parametros
@@ -66,7 +99,7 @@ promise().catch(function(message){
 // já o reject() chama o método catch()
 
 
-//fazendo uma request http com promise:
+//fazendo uma request http fetch, fetch retorna uma promise:
 window.fetch('https://viacep.com.br/ws/01001000/json').then(function(data){
     data.json().then(function(endereco){
         console.log(endereco)
